55e694af8d65b78f70b4be0125c45809732020f6
Jason Burns <jasonb315@protonmail.com>
2018-05-14 20:15:03 +0000

    config files updated

--- __test__/lib/array_shift.test.js
+++ __test__/lib/array_shift.test.js
@@ -3,7 +3,7 @@
 const arrayShift = require('../../array_shift.js'); //
 const assert = require('assert'); 
 
-it('insertShift() puts the value in the middle index', () => {
+it('315: insertShift() inserts properly', () => {
   let testArray = arrayShift([1, 3, 2, 4], 7);
   expect( testArray ).toEqual([1, 3, 7, 2, 4]);
 });
