434891b2486ce88dbe5ca68318a1d6157fb96389
Jason Burns <jasonb315@protonmail.com>
2018-05-14 20:53:28 +0000

    syntax cleanup 2

--- __test__/lib/array_shift.test.js
+++ __test__/lib/array_shift.test.js
@@ -1,7 +1,8 @@
 'use strict';
 
+
+
 const arrayShift = require('../../array_shift.js'); //
-const assert = require('assert'); 
 
 it('315: insertShift() inserts properly', () => {
   let testArray = arrayShift([1, 3, 2, 4], 7);
