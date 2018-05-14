b9e0eef16f348de0d2faf6c28036c347e35d7fc5
Jason Burns <jasonb315@protonmail.com>
2018-05-14 18:21:13 +0000

    BINARY search

--- __test__/lib/array_binary_search.test.js
+++ __test__/lib/array_binary_search.test.js
@@ -3,6 +3,6 @@
 const arrayBS = require('../../array_binary_search.js'); //
 console.log(arrayBS.BinarySearch);
 it('315: BinarySearch() inserts properly', () => {
-  let testArray = arrayBS.BinarySearch([1,2,3,4,5,6,7,8,9], 7);
-  expect( testArray ).toEqual(6);
+  let testArray = arrayBS.BinarySearch([4,8,15,16,23,42], 12);
+  expect( testArray ).toEqual(-1);
 });
--- array_binary_search.js
+++ array_binary_search.js
@@ -1,13 +1,43 @@
 'use strict';
 
-module.exports = exports = {};
+var myArr = [4,8,15,16,23,42];
 
-exports.BinarySearch = (array, keyVal) => {
-  for(var i = 0 ; i < array.length ; i++){
-    if ( array[i] === keyVal ){
-      return i;
-    }
-  }
-  return -1;
+var binarySearch = function(arr, val) {
+   var half = Math.floor(arr.length / 2);
+ 
+   if (arr.length === 1) {
+       if (arr[0] === val) {
+           return val;
+       } else {
+           return -1;
+       }
 
-};
+   } else if (arr[half] === val) {
+     console.log(“arr[half]: “, arr[half]);
+     return val;
+
+   } else if (val > arr[half]) {
+     console.log(“arr[half]: “, arr[half]);
+     return binarySearch(arr.createNewArray(half, arr.length), val);
+
+   } else {
+     console.log(“arr[half]: “, arr[half]);
+     return binarySearch(arr.createNewArray(0, half), val);
+   }
+ }
+
+
+Array.prototype.createNewArray = function (startingPoint, endingPoint) {
+   const newArray = [];
+   var newStartingPoint = 0;
+
+   for (var i = startingPoint; i < endingPoint; i++) {
+       newArray[newStartingPoint] = this[i];
+       newStartingPoint++;
+   }
+
+   console.log(`new array: ${newArray}`);
+   return newArray;
+}
+
+console.log(binarySearch(myArr, 12));
