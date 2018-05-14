f01c20933cb983a08ebe5b500c1baffc83812694
Jason Burns <jasonb315@protonmail.com>
2018-05-14 16:27:26 +0000

    test for function assignment

--- __test__/lib/array_binary_search.test.js
+++ __test__/lib/array_binary_search.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const arrayBS = require('../../array_binary_search.js'); //
-
+console.log(arrayBS);
 it('315: BinarySearch() inserts properly', () => {
   let testArray = arrayBS([1,2,3,4,5,6,7,8,9], 7);
   expect( testArray ).toEqual(6);
