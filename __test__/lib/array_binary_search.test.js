bc57f70ebaa2b9e01fe36e450d8e61315f0c7ad7
Jason Burns <jasonb315@protonmail.com>
2018-05-14 19:07:09 +0000

    tests modified

--- __test__/lib/array_binary_search.test.js
+++ __test__/lib/array_binary_search.test.js
@@ -2,22 +2,26 @@
 
 const arrayBS = require('../../array_binary_search.js'); //
 
-it('315: BinarySearch() inserts properly', () => {
-  let testArray = arrayBS.binarySearch([4,8,15,16,23,42], 12);
-  expect( testArray ).toEqual(-1);
-});
+describe('ArrayBinarySearch', () => {
 
-it('315: BinarySearch() inserts properly', () => {
-  let testArray = arrayBS.binarySearch([4,8,15,16,23,42], 4);
-  expect( testArray ).toEqual(0);
-});
+  it('315: BinarySearch() inserts properly', () => {
+    let testArray = arrayBS.binarySearch([4,8,15,16,23,42], 12);
+    expect( testArray ).toEqual(-1);
+  });
 
-it('315: BinarySearch() inserts properly', () => {
-  let testArray = arrayBS.binarySearch([4,8,15,16,23,42], true);
-  expect( testArray ).toEqual(-1);
-});
+  it('315: BinarySearch() inserts properly', () => {
+    let testArray = arrayBS.binarySearch([4,8,15,16,23,42], 4);
+    expect( testArray ).toEqual(4);
+  });
 
-it('315: BinarySearch() inserts properly', () => {
-  let testArray = arrayBS.binarySearch([4,8,15,16,23,42], false);
-  expect( testArray ).toEqual(-1);
+  it('315: BinarySearch() inserts properly', () => {
+    let testArray = arrayBS.binarySearch([4,8,15,16,23,42], true);
+    expect( testArray ).toEqual(-1);
+  });
+
+  it('315: BinarySearch() inserts properly', () => {
+    let testArray = arrayBS.binarySearch([4,8,15,16,23,42], false);
+    expect( testArray ).toEqual(-1);
+  });
+
 });
