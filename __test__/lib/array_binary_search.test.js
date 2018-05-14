b389a53a4766f83a6a5176ec6f9ba7f20e9f2fa7
Jason Burns <jasonb315@protonmail.com>
2018-05-14 18:34:20 +0000

    naming correction

--- __test__/lib/array_binary_search.test.js
+++ __test__/lib/array_binary_search.test.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const arrayBS = require('../../array_binary_search.js'); //
-console.log(arrayBS.BinarySearch);
+
 it('315: BinarySearch() inserts properly', () => {
-  let testArray = arrayBS.BinarySearch([4,8,15,16,23,42], 12);
+  let testArray = arrayBS.binarySearch([4,8,15,16,23,42], 12);
   expect( testArray ).toEqual(-1);
 });
