1b3e13e59f79493b7dc868b59cd113d828e316c3
Jason Burns <jasonb315@protonmail.com>
2018-05-14 18:25:24 +0000

    BINARY search cleanup

--- array_binary_search.js
+++ array_binary_search.js
@@ -1,43 +1,35 @@
 'use strict';
 
-var myArr = [4,8,15,16,23,42];
-
 var binarySearch = function(arr, val) {
-   var half = Math.floor(arr.length / 2);
- 
-   if (arr.length === 1) {
-       if (arr[0] === val) {
-           return val;
-       } else {
-           return -1;
-       }
+  var half = Math.floor(arr.length / 2);
 
-   } else if (arr[half] === val) {
-     console.log(“arr[half]: “, arr[half]);
-     return val;
+  if (arr.length === 1) {
+    if (arr[0] === val) {
+      return val;
+    } else {
+      return -1;
+    }
 
-   } else if (val > arr[half]) {
-     console.log(“arr[half]: “, arr[half]);
-     return binarySearch(arr.createNewArray(half, arr.length), val);
+  } else if (arr[half] === val) {
+    return val;
 
-   } else {
-     console.log(“arr[half]: “, arr[half]);
-     return binarySearch(arr.createNewArray(0, half), val);
-   }
- }
+  } else if (val > arr[half]) {
+    return binarySearch(arr.createNewArray(half, arr.length), val);
+
+  } else {
+    return binarySearch(arr.createNewArray(0, half), val);
+  }
+};
 
 
 Array.prototype.createNewArray = function (startingPoint, endingPoint) {
-   const newArray = [];
-   var newStartingPoint = 0;
+  const newArray = [];
+  var newStartingPoint = 0;
 
-   for (var i = startingPoint; i < endingPoint; i++) {
-       newArray[newStartingPoint] = this[i];
-       newStartingPoint++;
-   }
+  for (var i = startingPoint; i < endingPoint; i++) {
+    newArray[newStartingPoint] = this[i];
+    newStartingPoint++;
+  }
 
-   console.log(`new array: ${newArray}`);
-   return newArray;
-}
-
-console.log(binarySearch(myArr, 12));
+  return newArray;
+};
