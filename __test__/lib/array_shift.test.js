2ce87f918bc7657da9f80bbf91233a3f5d4db77d
Jason Burns <jasonb315@protonmail.com>
2018-05-14 19:20:25 +0000

    jest connected

---
+++ __test__/lib/array_shift.test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const arrayShift = require('../../lib/array_shift.js'); //
+const assert = require('assert'); 
+
+let thing = 0;
+
+results = arithmetic.add(1,1);
+assert.strictEqual(results, expected, '315:');
