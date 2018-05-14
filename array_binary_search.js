3adcdda2f2eceaead0b434817f72eb2a74da58eb
Jason Burns <jasonb315@protonmail.com>
2018-05-14 15:37:17 +0000

    array binary search function whiteboard ad Fx

---
+++ array_binary_search.js
@@ -0,0 +1,19 @@
+'use strict';
+
+
+//a should be a sorted array
+//b should be a search key
+
+function BinarySearch(a, b){
+  for(var i = 0 ; i < a.length ; i++){
+    if ( a[i] === b ){
+      return i;
+    } else {
+      // return -1;
+    }
+  }
+
+
+}
+
+BinarySearch();
