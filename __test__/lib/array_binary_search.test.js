226a8a7bf35cefc2e1fd1b68eab36d27f1769a1a
Jason Burns <jasonb315@protonmail.com>
2018-05-14 18:56:58 +0000

    tests added

--- __test__/lib/array_binary_search.test.js
+++ __test__/lib/array_binary_search.test.js
@@ -6,3 +6,18 @@ it('315: BinarySearch() inserts properly', () => {
   let testArray = arrayBS.binarySearch([4,8,15,16,23,42], 12);
   expect( testArray ).toEqual(-1);
 });
+
+it('315: BinarySearch() inserts properly', () => {
+  let testArray = arrayBS.binarySearch([4,8,15,16,23,42], 4);
+  expect( testArray ).toEqual(0);
+});
+
+it('315: BinarySearch() inserts properly', () => {
+  let testArray = arrayBS.binarySearch([4,8,15,16,23,42], true);
+  expect( testArray ).toEqual(-1);
+});
+
+it('315: BinarySearch() inserts properly', () => {
+  let testArray = arrayBS.binarySearch([4,8,15,16,23,42], false);
+  expect( testArray ).toEqual(-1);
+});
