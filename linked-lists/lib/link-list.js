78a37ee7b2d60e5287cbabbf0408c99c8931beea
Jason Burns <jasonb315@protonmail.com>
2018-05-19 22:07:38 +0000

    remove by val, remove by index

--- linked-lists/lib/link-list.js
+++ linked-lists/lib/link-list.js
@@ -12,7 +12,7 @@ class LinkedList {
 
   }
 
-  //NODE REFRENCE
+  //NODE.JS REFRENCE
 
   // class Node {
   //   constructor(value) {
@@ -25,7 +25,6 @@ class LinkedList {
     
     let node = new Node(value);
 
-    // This happens if the list is empty
     if (!this.head) {
 
       this.head = node;
@@ -62,30 +61,142 @@ class LinkedList {
 
   }//Fx prepend
 
-  remove(offset) {
+  removeByValue(offsetVal) {
 
     let currentNode = this.head;
     let previousNode = currentNode;
  
-    if(currentNode.value === offset){
+    if(currentNode.value === offsetVal){
 
+      //if HEAD is being removed
       this.head = currentNode.next;
 
     } else {
 
-      while(currentNode.value !== offset){
+      while(currentNode.value !== offsetVal){
         previousNode = currentNode;
         currentNode = currentNode.next;
 
       }
 
+      //connect across/through current node after val match found
       previousNode.next = currentNode.next;
 
     }
 
     this.length--;
 
-  }//F remove
+  }//F removeByVal
+
+  removeByIndex(index){
+
+    var currentNode = this.head;
+
+    var previousNode;
+
+    var currentIndex = 0;
+
+    if (index === 0){
+
+      this.head = currentNode.next;
+
+    } else {
+
+      while(currentIndex < index){
+        currentIndex++;
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+      }
+      previousNode.next = currentNode.next;
+
+    }
+    this.length--;
+    return currentNode.value;
+
+//TODO^^^^^^^^^
+
+
+
+  }//Fx removeByIndex
+
+  lengthOf(){
+
+    console.log(`this.length: ${this.length}`);
+
+    return this.length;
+
+  }//Fx lengthOf
+
+  findIndex(searchVal){
+    //search input by value key
+
+    var currentNode = this.head;
+
+    var index = -1;
+
+    while(currentNode){
+
+      index++;
+
+      if(currentNode.value === searchVal){
+        return index;
+      }
+
+      currentNode = currentNode.next;
+
+    }
+
+    return -1;
+    //if not found
+
+  }//Fx findIndex
+
+  insertNode(index, value){
+
+    let node = new Node(value);
+
+    var currentNode = this.head;
+
+    var previousNode;
+
+    var currentIndex = 0;
+
+    if(index > this.length){
+
+      console.log('Index argument passed is larger than list length.');
+      return false;
+
+    }
+
+    if(index === 0){
+      //user is inserting node as new HEAD
+
+      node.next = currentNode;
+
+      this.head = node;
+
+    } else {
+
+      while(currentIndex < index){
+
+        currentIndex++;
+
+        previousNode = currentNode;//c becomes p in next line
+
+        currentNode = currentNode.next;//move down line until current node === index
+
+      }
+
+      node.next = currentNode;
+
+      previousNode.next = node;
+
+    }
+    this.length++;
+  }//Fx insertNode
+
+
+
 
   //   for (var i = 0; i < this.length; i++) {
   //     if (offset == i) {
