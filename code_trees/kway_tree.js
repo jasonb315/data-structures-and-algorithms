2794edc2ed19c7dc9d73842ec52d12c9683ef2f6
Jason Burns <jasonb315@protonmail.com>
2018-06-21 14:17:16 -0700

    kway work

--- code_trees/kway_tree.js
+++ code_trees/kway_tree.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import Node from './kway_node.js';
+const Node = require ('./kway_node.js');
 
     class KWayTree {
 
@@ -8,9 +8,37 @@ import Node from './kway_node.js';
             this.root = null;
         }
 
-        appendChild(value){
-            const nodeToAppend = new KWayNode(value);
-            this.children.push(nodeToAppend);
+        proGen(genVal){
+            const node = new Node(genVal);
+            this.root = node;
+        }
+
+        reGen(genVal, stemFrom){
+           
+            //new node
+            const newNode = new Node(genVal);
+
+            // starting point
+            let currentNode = this.root;
+
+           
+            if (currentNode.value !== stemFrom){
+
+            }
+            
+
+            //if proGen not called, make reGen root.
+            if (currentNode === null){
+                this.root = newNode;
+            }//
+
+            //move down tree to find specified stem to leaf.
+            if(currentNode.value === stemFrom){
+
+            }
+            
+            this.children.push(node);
+            
           }
 
         breadthFirstTraversal(){
@@ -30,3 +58,17 @@ import Node from './kway_node.js';
     }
 
     module.exports = KWayTree;
+
+
+
+//     Latin prefix	Basic meaning	Example words
+// co-	together	coauthor, coedit, coheir
+// de-	away, off; generally indicates reversal or removal in English	deactivate, debone, defrost, decompress, deplane
+// dis-	not, not any	disbelief, discomfort, discredit, disrepair, disrespect
+// inter-	between, among	international, interfaith, intertwine, intercellular, interject
+// non-	not	nonessential, nonmetallic, nonresident, nonviolence, nonskid, nonstop
+// post-	after	postdate, postwar, postnasal, postnatal
+// pre-	before	preconceive, preexist, premeditate, predispose, prepossess, prepay
+// re-	again; back, backward	rearrange, rebuild, recall, remake, rerun, rewrite
+// sub-	under	submarine, subsoil, subway, subhuman, substandard
+// trans-	across, beyond, through	transatlantic, transpolar
